7f0856e06f4223ecc1dd732c7ad02c465e1ca1b0
Jun Furuse <jun.furuse@gmail.com>
2013-01-13 19:53:40 -0800

    test fix

---
+++ tests/ocamlbuild/dir/dir2/bye.ml
@@ -0,0 +1,2 @@
+let bye = "Bye."
+
--- tests/ocamlbuild/hello.ml
+++ tests/ocamlbuild/hello.ml
@@ -1,5 +1,6 @@
 let _ =
-  Printf.printf "Hello, %s ! My name is %s\n"
+  Printf.printf "Hello, %s ! My name is %s! %s!\n"
     (if Array.length Sys.argv > 1 then Sys.argv.(1) else "stranger")
     Sys.argv.(0)
+    Bye.bye
 ;;
